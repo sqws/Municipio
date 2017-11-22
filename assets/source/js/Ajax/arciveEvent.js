8e8ceec9a3987af946bf1421873f4491317e6124
Simon Quick <simon.quick@whitespace.se>
2017-11-22 14:07:31 +0100

    Removed upload status bar check

--- assets/source/js/Ajax/arciveEvent.js
+++ assets/source/js/Ajax/arciveEvent.js
@@ -66,29 +66,6 @@ Muncipio.Ajax.ArchiveEvent = (function ($) {
         this.$eventArchive.addClass('loading');
 
         $.ajax({
-            xhr: function() {
-                var xhr = new window.XMLHttpRequest();
-        
-                // Upload progress
-                xhr.upload.addEventListener("progress", function(evt){
-                    if (evt.lengthComputable) {
-                        var percentComplete = evt.loaded / evt.total;
-                        //Do something with upload progress
-                        console.log(percentComplete);
-                    }
-               }, false);
-        
-               // Download progress
-               xhr.addEventListener("progress", function(evt){
-                   if (evt.lengthComputable) {
-                       var percentComplete = evt.loaded / evt.total;
-                       // Do something with download progress
-                       console.log(percentComplete);
-                   }
-               }, false);
-        
-               return xhr;
-            },
             url: ajaxurl,
             data: data,
             type: 'get',
